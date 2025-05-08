8c644b47ff683f87915193c484fae53edd743f65
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-05-08 15:00:07 +0200

    adding 404 page

---
+++ wp-content/themes/vm/404.php
@@ -0,0 +1,12 @@
+<!--PAGE QUI EST BONNE FERTIG-->
+
+<?php get_header(); ?>
+<div class="not-found">
+    <h1 class="not-found__title">Page non trouvée</h1>
+    <p class="not-found__message">Désolé, la page que vous recherchez n'existe pas ou a été déplacée.</p>
+    <p class="not-found__message">
+      Retour à la
+        <a href="<?= home_url(); ?>" title="Retour à la page d'accueil" class="not-found__link">
+            page d’accueil.
+        </a>
+<?php get_footer(); ?>
