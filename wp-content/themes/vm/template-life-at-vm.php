c2618079a24c118190840b8199524d78bc9542bf
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-05-26 00:31:49 +0200

    changing structure of life-at-vm

--- wp-content/themes/vm/template-life-at-vm.php
+++ wp-content/themes/vm/template-life-at-vm.php
@@ -17,11 +17,12 @@
 </section>
 
 <section class="activities_container img_title_text">
-    <h2 class="section_title">
-        <span class="underline">Activités</span> des enfants
-    </h2>
-    <?= get_field('activities_text');?>
-
+    <div class="text_content">
+        <h2 class="section_title">
+            <span class="underline">Activités</span> des enfants
+        </h2>
+        <?= get_field('activities_text'); ?>
+    </div>
     <div class="life_activity_image">
         <?php
         $life_activity_image = get_field('activity_image');
@@ -35,18 +36,102 @@
 
 
 <!--HEEEELP -->
-
-
-    <section class="news_section">
+<!--    <section class="news_section">
         <h2 class="section_title title_middle">
-            <span class="underline"><?= get_field('title_news');?></span>
+        <span class="underline">
+            <?php /*= get_field('title_news');*/?>
+        </span>
         </h2>
-        <a href="<?= get_field('link_vie_to_news');?>" title="Aller vers la page 'Actualités'" class="link_to_page. link">Voir toutes les actualités
+        <a href="<?php /*= get_field('link_vie_to_news');*/?>" title="Aller vers la page 'Actualités'"
+           class="link_to_page. link">Voir toutes les actualités
             <svg width="13" height="8" viewBox="0 0 13 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M12.1638 4.35355C12.3591 4.15829 12.3591 3.84171 12.1638 3.64645L8.98185 0.464467C8.78659 0.269204 8.47001 0.269204 8.27475 0.464467C8.07948 0.659729 8.07948 0.976311 8.27475 1.17157L11.1032 4L8.27475 6.82843C8.07948 7.02369 8.07948 7.34027 8.27475 7.53553C8.47001 7.7308 8.78659 7.7308 8.98185 7.53553L12.1638 4.35355ZM0.853516 4L0.853516 4.5L11.8103 4.5L11.8103 4L11.8103 3.5L0.853516 3.5L0.853516 4Z" fill="#F19595"/>
+                <path
+                        d="M12.1638 4.35355C12.3591 4.15829 12.3591 3.84171 12.1638 3.64645L8.98185 0.464467C8.78659 0.269204 8.47001 0.269204 8.27475 0.464467C8.07948 0.659729 8.07948 0.976311 8.27475 1.17157L11.1032 4L8.27475 6.82843C8.07948 7.02369 8.07948 7.34027 8.27475 7.53553C8.47001 7.7308 8.78659 7.7308 8.98185 7.53553L12.1638 4.35355ZM0.853516 4L0.853516 4.5L11.8103 4.5L11.8103 4L11.8103 3.5L0.853516 3.5L0.853516 4Z"
+                        fill="#F19595" />
             </svg>
         </a>
 
+        <div class="news">
+            <?php
+/*            $news = new WP_Query([
+                'post_type' => 'news',
+                'order' => 'DESC',
+                'orderby' => 'date',
+                'posts_per_page' => 3,
+            ]);
+
+            if($news->have_posts()): while($news->have_posts()): $news->the_post(); */?>
+                <article class="news__card">
+                    <h3 class="news__title">
+                        <?php /*= get_the_title(); */?>
+                    </h3>
+                    <a href="<?php /*= get_the_permalink(); */?>" class="news__link link">
+                <span class="sro">Découvrir l'actualité
+                    <?php /*= get_the_title(); */?>
+                </span>
+                    </a>
+                    <div class="news__dates">
+                        <?php
+/*                        $date = get_field('news_date');
+                        if ($date):
+                            */?>
+                            <p class="single_news_date">
+                                <time datetime="<?php /*= $date; */?>">
+                                    <?php /*= $date; */?>
+                                </time>
+                            </p>
+                        <?php /*else: */?>
+                            <p> Sans date </p>
+                        <?php /*endif; */?>
+                    </div>
+                    <div class="news_button">
+                <span>
+                    <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"
+                         class="news_button_circle">
+                        <rect width="48" height="48" rx="24" fill="#F19595" />
+                    </svg>
+                    <svg width="14" height="22" viewBox="0 0 14 22" fill="none" xmlns="http://www.w3.org/2000/svg"
+                         class="news_button_arrow">
+                        <path d="M1.5 1.5L11 11L1.5 20.5" stroke="white" stroke-width="3" stroke-linecap="round" />
+                    </svg>
+                </span>
+                    </div>
+                    <figure class="news__fig">
+                        <?php /*= responsive_image(get_field('news_image'), ['classes' => 'news__img']);*/?>
+                    </figure>
+                    <a href="<?php /*= the_permalink() */?>">Lien</a>
+                </article>
+                <?php
+/*                wp_reset_postdata();
+            endwhile; else: */?>
+                <p>Je n'ai pas d'actualités récentes à montrer...</p>
+            <?php /*endif; */?>
+        </div>
+    </section>-->
+    <section class="news_section">
+        <div class="news_header">
+            <h2 class="section_title back">Nos <span class="underline back">actualités</span>
+            </h2>
+            <a href="<?= get_field('link_homepage_to_news'); ?>" title="Aller vers la page 'Actualités'"
+               class="link show_all_news">Voir
+                toutes les actualités
+                <svg width="13" height="8" viewBox="0 0 13 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                            d="M12.1638 4.35355C12.3591 4.15829 12.3591 3.84171 12.1638 3.64645L8.98185 0.464467C8.78659 0.269204 8.47001 0.269204 8.27475 0.464467C8.07948 0.659729 8.07948 0.976311 8.27475 1.17157L11.1032 4L8.27475 6.82843C8.07948 7.02369 8.07948 7.34027 8.27475 7.53553C8.47001 7.7308 8.78659 7.7308 8.98185 7.53553L12.1638 4.35355ZM0.853516 4L0.853516 4.5L11.8103 4.5L11.8103 4L11.8103 3.5L0.853516 3.5L0.853516 4Z"
+                            fill="#F19595"/>
+                </svg>
+            </a></div>
+<!--        <h2 class="section_title title_middle back">
+            Nos <span class="underline">actualités</span>
+        </h2>
+        <a href="<?php /*= get_field('link_homepage_to_news'); */?>" title="Aller vers la page 'Actualités'" class="link show_all_news">Voir
+            toutes les actualités
+            <svg width="13" height="8" viewBox="0 0 13 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                        d="M12.1638 4.35355C12.3591 4.15829 12.3591 3.84171 12.1638 3.64645L8.98185 0.464467C8.78659 0.269204 8.47001 0.269204 8.27475 0.464467C8.07948 0.659729 8.07948 0.976311 8.27475 1.17157L11.1032 4L8.27475 6.82843C8.07948 7.02369 8.07948 7.34027 8.27475 7.53553C8.47001 7.7308 8.78659 7.7308 8.98185 7.53553L12.1638 4.35355ZM0.853516 4L0.853516 4.5L11.8103 4.5L11.8103 4L11.8103 3.5L0.853516 3.5L0.853516 4Z"
+                        fill="#F19595" />
+            </svg>
+        </a>-->
         <div class="news">
             <?php
             $news = new WP_Query([
@@ -56,55 +141,62 @@
                 'posts_per_page' => 3,
             ]);
 
-            if($news->have_posts()): while($news->have_posts()): $news->the_post(); ?>
-                <article class="news">
-                    <a href="<?= get_the_permalink(); ?>" class="news__link link">
-                        <span class="sro">Découvrir l'actualité<?= get_the_title(); ?></span>
+            // On ajoute les actualités sur la page
+            // On ouvre "la boucle" (The Loop), la structure de contrôle
+            // de contenu propre à Wordpress:
+
+
+            if ($news->have_posts()): while ($news->have_posts()): $news->the_post(); ?>
+                <article class="single_news news__card">
+                    <h3 class="news__title">
+                        <?= get_the_title(); ?>
+                    </h3>
+
+                    <a href="<?= get_the_permalink(); ?>" class="news__link" title="Aller vers le projet">
+                        <span class="sro">Découvrir l'actualité <?= get_the_title(); ?></span>
                     </a>
+                    <div class="news__dates">
 
-
-                    <!--                 <div class="project__card">
-                    <div class="project__head">
-                        <h3 class="project__title"><?php /*= get_the_title(); */?></h3>
-                    </div>
-
-
-
-                    <figure class="project__fig">
-                        <?php /*= get_the_post_thumbnail(size: 'medium', attr: ['class' => 'project__img']);
-                        */?>
-                    </figure>
-                </div>-->
-
-                    <div class="news__card">
-                        <div class="news__head">
-                            <h3 class="news__title"><?= get_the_title(); ?></h3>
-                        </div>
-                        <figure class="news__fig">
-                            <?= responsive_image(get_field('news_image'), ['classes' => 'news__img']);?>
-
-                            <!--        --><?php /*= get_the_post_thumbnail(size: 'medium', attr: ['class' => 'news__img']); */?>
-                        </figure>
                         <?php
                         $date = get_field('news_date');
-                        ?>
-                        <p class="single_news_date">
-                            <time datetime="<?= $date; ?>">
-                                <?= $date; ?>
-                            </time>
-                        </p>
+                        if ($date):
+                            ?>
+                            <p class="single_news_date">
+                                <time datetime="<?= $date; ?>">
+                                    <?= $date; ?>
+                                </time>
+                            </p>
+                        <?php else: ?>
+                            <p> Sans date </p>
+                        <?php endif; ?>
+                    </div>
+                    <div class="news_button">
+                    <span>
+                        <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"
+                             class="news_button_circle">
+                            <rect width="48" height="48" rx="24" fill="#F19595" />
+                        </svg>
+                        <svg width="14" height="22" viewBox="0 0 14 22" fill="none" xmlns="http://www.w3.org/2000/svg"
+                             class="news_button_arrow">
+                            <path d="M1.5 1.5L11 11L1.5 20.5" stroke="white" stroke-width="3" stroke-linecap="round" />
+                        </svg>
+                    </span>
                     </div>
 
+                    <div class="news__fig">
 
+                        <?= responsive_image(get_field('news_image'), ['classes' => 'news__img']); ?>
+
+                    </div>
                 </article>
                 <?php
                 wp_reset_postdata();
-            endwhile; else: ?>
-                <p>Je n'ai pas d'actualités récentes à montrer...</p>
+            endwhile;
+            else: ?>
+                <p>Je n'ai pas d'actualités récentes à montrer pour le moment...</p>
             <?php endif; ?>
         </div>
     </section>
-
 
 
 
@@ -112,30 +204,32 @@
         <h2 class="section_title title_middle">
             <span class="underline">Témoignages</span> de nos enfants
         </h2>
-        <div class="single_textimonial">
-            <span>“</span>
-            <?= get_field('first_testimonial_text'); ?>
-            <span class="end_quote">”</span>
+        <div class="testimonials_flex_container">
+            <div class="single_textimonial">
+                <span>“</span>
+                <?= get_field('first_testimonial_text'); ?>
+                <span class="end_quote">”</span>
 
-            <p class="name_age">
-                <?= get_field('name_age_first_testimonial'); ?>
-            </p>
-        </div>
-        <div class="single_textimonial">
-            <span>“</span>
-            <?= get_field('second_testimonial_text'); ?>
-            <span class="end_quote">”</span>
-            <p class="name_age">
-                <?= get_field('name_age_second_testimonial'); ?>
-            </p>
-        </div>
-        <div class="single_textimonial">
-            <span>“</span>
-            <?= get_field('third_testimonial_text'); ?>
-            <span class="end_quote">”</span>
-            <p class="name_age">
-                <?= get_field('name_age_third_testimonial'); ?>
-            </p>
+                <p class="name_age">
+                    <?= get_field('name_age_first_testimonial'); ?>
+                </p>
+            </div>
+            <div class="single_textimonial">
+                <span>“</span>
+                <?= get_field('second_testimonial_text'); ?>
+                <span class="end_quote">”</span>
+                <p class="name_age">
+                    <?= get_field('name_age_second_testimonial'); ?>
+                </p>
+            </div>
+            <div class="single_textimonial">
+                <span>“</span>
+                <?= get_field('third_testimonial_text'); ?>
+                <span class="end_quote">”</span>
+                <p class="name_age">
+                    <?= get_field('name_age_third_testimonial'); ?>
+                </p>
+            </div>
         </div>
     </section>
 
@@ -187,7 +281,7 @@
     <!--Boucle sur le repeater "faq_life" pour afficher les questions et réponses-->
     <?php
     if (have_rows('faq_family')): ?>
-        <ul class="faq_life_list">
+        <ul class="faq_life_list faq_family_list">
             <?php while (have_rows('faq_family')): the_row();
 
                 // Récupère les sous-champs
