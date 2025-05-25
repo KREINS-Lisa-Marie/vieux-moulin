e8349dce42c3681a361c7f91778db6b44b6f871b
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-05-26 00:08:38 +0200

    adding scss and improving structure

--- wp-content/themes/vm/template-houses.php
+++ wp-content/themes/vm/template-houses.php
@@ -24,12 +24,13 @@
     </h2>
 
     <section class="houses_vm_introduction_container img_title_text">
-        <h3 class="section_title">
-            <span class="underline">Le </span>
-            <span class="underline">Vieux </span>
-            <span class="underline">Moulin</span>
-        </h3>
-        <?= get_field('text_vieux_moulin'); ?>
+        <div class="text_content">
+            <h3 class="section_title">
+                <span class="underline">Le </span>
+                <span class="underline">Vieux </span>
+                <span class="underline">Moulin</span>
+            </h3>
+            <?= get_field('text_vieux_moulin'); ?></div>
 
         <div class="image_vm">
             <?php
@@ -43,10 +44,11 @@
     </section>
 
     <section class="houses_vm_team_container img_title_text">
-        <h3 class="section_title">
-            <span class="underline">L'équipe</span>
-        </h3>
-        <?= get_field('text_team_vieux_moulin'); ?>
+        <div class="text_content">
+            <h3 class="section_title">
+                <span class="underline">L'équipe</span>
+            </h3>
+            <?= get_field('text_team_vieux_moulin'); ?></div>
 
         <div class="image_vm">
             <?php
@@ -92,10 +94,11 @@
     </h2>
 
     <section class="houses_e_introduction_container img_title_text">
-        <h3 class="section_title">
-            <span class="underline">Edelweiss</span>
-        </h3>
-        <?= get_field('text_edelweiss'); ?>
+        <div class="text_content">
+            <h3 class="section_title">
+                <span class="underline">Edelweiss</span>
+            </h3>
+            <?= get_field('text_edelweiss'); ?></div>
 
         <div class="image_e">
             <?php
@@ -109,10 +112,11 @@
     </section>
 
     <section class="houses_e_team_container img_title_text">
-        <h3 class="section_title">
-            <span class="underline">L'équipe</span>
-        </h3>
-        <?= get_field('text_team_edelweiss'); ?>
+        <div class="text_content">
+            <h3 class="section_title">
+                <span class="underline">L'équipe</span>
+            </h3>
+            <?= get_field('text_team_edelweiss'); ?></div>
 
         <div class="image_e">
             <?php
@@ -171,14 +175,15 @@
 
 
                 if ($p_image && $p_title && $p_text): ?>
-                <div class="img_title_text">
+                <div class="single_project img_title_text">
 
 
-                    <h3 class="project_title">
-                        <?= $p_title ?>
-                    </h3>
-                    <?= $p_text ?>
-
+                    <div class="text_content">
+                        <h3 class="project_title">
+                            <?= $p_title ?>
+                        </h3>
+                        <?= $p_text ?>
+                    </div>
                     <div class="project_image">
                         <?php
                         $size = 'full'; // (thumbnail, medium, large, full or custom size)
@@ -216,7 +221,7 @@
     <div class="donations_subline">
         <?= get_field('subline'); ?>
     </div>
-    <a href="<?= get_field('link_houses_to_contact');?>" title="Aller vers la page 'Contact'" class="link_to_page link">Dites nous comment
+    <a href="<?= get_field('link_houses_to_contact');?>" title="Aller vers la page 'Contact'" class="link_to_page link link_to_donation_contact">Dites nous comment
         <svg width="13" height="8" viewBox="0 0 13 8" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M12.1638 4.35355C12.3591 4.15829 12.3591 3.84171 12.1638 3.64645L8.98185 0.464467C8.78659 0.269204 8.47001 0.269204 8.27475 0.464467C8.07948 0.659729 8.07948 0.976311 8.27475 1.17157L11.1032 4L8.27475 6.82843C8.07948 7.02369 8.07948 7.34027 8.27475 7.53553C8.47001 7.7308 8.78659 7.7308 8.98185 7.53553L12.1638 4.35355ZM0.853516 4L0.853516 4.5L11.8103 4.5L11.8103 4L11.8103 3.5L0.853516 3.5L0.853516 4Z"
                   fill="#F19595"/>
